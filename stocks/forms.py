1f1df774c944c8b1fc0dbf2b33cb396c54997e04
Yaseryl <yaserylaurian@gmail.com>
2024-08-09 15:20:34 +0100

    Add Sign Up Form test and add validation to sign up form

--- stocks/forms.py
+++ stocks/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 from .models import User
 class SignUpForm(forms.Form):
-    username = forms.CharField(label='username')
+    username = forms.CharField(label='username', max_length=30)
     new_password = forms.CharField(label='password', widget=forms.PasswordInput())
     password_confirmation = forms.CharField(label='password confirmation', widget=forms.PasswordInput())
+
+    def clean(self):
+        super().clean()
+        new_password = self.cleaned_data.get('new_password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+        if new_password != password_confirmation:
+            self.add_error('password_confirmation', "Confirmation does not match password.")
---
+++ stocks/tests/forms/test_sign_up_form.py
@@ -0,0 +1,43 @@
+"""Unit tests of the sign up form"""
+from django.test import TestCase
+from stocks.forms import SignUpForm
+from stocks.models import User
+from django import forms
+
+class SignUpFormTestCase(TestCase):
+
+    def setUp(self):
+        self.form_input = {
+            "username": "johndoe",
+            "new_password": "Password123",
+            "password_confirmation": "Password123"
+        }
+
+    def test_valid_sign_up_form(self):
+        
+        form = SignUpForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+    
+    def test_form_has_necessary_fields(self):
+        form = SignUpForm()
+        self.assertIn('username', form.fields)
+        self.assertIn('new_password', form.fields)
+        self.assertIn('password_confirmation', form.fields)
+
+    def test_password_field_widget(self):
+        form = SignUpForm()
+        new_password_widget = form.fields['new_password'].widget
+        self.assertTrue(isinstance(new_password_widget, forms.PasswordInput))
+
+        password_confirm_widget = form.fields['password_confirmation'].widget
+        self.assertTrue(isinstance(password_confirm_widget, forms.PasswordInput))
+
+    def test_form_uses_model_validation(self):
+        self.form_input['username'] = 'b'*31
+        form = SignUpForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+    
+    def test_new_password_and_password_confirmation_are_indentical(self):
+        self.form_input['password_confirmation'] = "WrongPassword123"
+        form = SignUpForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
